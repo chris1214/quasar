936b859945c3f5ab748134e12b8d5ef80e4d8bab
Christopher andrio <christopher_andrio@hotmail.com>
2017-05-08 14:08:29 -0300

    Arrumando tabela e indentação do codigo

--- quasar-01/src/components/Repositories.vue
+++ quasar-01/src/components/Repositories.vue
@@ -13,6 +13,225 @@
       </q-toolbar-title>
     </div>
 
+
+    <div class="layout-view">
+      <div class="card">
+        <div class="card-content">
+          <p class="caption">Detalhes</p>
+
+          <div class="row">
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey">Nome do Usuario:</span>
+                <p>Gustavo</p>
+              </div>
+            </div>
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey">Tipo de Documento:</span>
+                <p>CPF</p>
+              </div>
+            </div>
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey">Validado no Workfinity?</span>
+                <p>Sim</p>
+              </div>
+            </div>
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey">CEP:</span>
+                <p>22666999</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="row">
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey"> Email:</span>
+                <p>Gustavo@hotmail.com</p>
+              </div>
+            </div>
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey">Número do Documento:</span>
+                <p>99638266015</p>
+              </div>
+            </div>
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey">Status:</span>
+                <p>Habilitado</p>
+              </div>
+            </div>
+
+            <div class="grow-2">
+              <div class="item-content">
+                <span class="text-grey">Contratante:</span>
+                <p>BANRISUL</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <!--<div class="card">
+        <div class="card-content">
+          <p class="caption">Detalhes</p>
+          <div class="list">
+            <div class="row">
+
+              <div class="grow-1">
+                <div class="item">
+                  <div class="item-content">
+                    Nome do Usuario: Gustavo
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    Tipo de documento: CPF
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    Validado no Workfinity? Sim
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    Email: Gustavo@hotmail.com
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    Número do Documento: 99638266015
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    Validado no Workfinity? Sim
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    Status:
+                    Habilitado
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    Contratante:
+                    BANRISUL
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="item">
+                  <div class="item-content">
+                    CEP:
+                    22666999
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>-->
+      <!--<div class="card">
+        <div class="card-content">
+          <p class="caption">Editar</p>
+          <div class="row gutter">
+
+            <div>
+              Nome de úsuario:
+              <input v-model="username" placeholder="Nome de úsuario">
+            </div>
+            <div>
+              Email: <input v-model="username" placeholder="Email">
+            </div>
+
+            <div>
+              Status:
+              <q-select
+                type="list"
+                v-model="select"
+                :options="selectOptions"
+              ></q-select>
+            </div>
+          </div>
+        </div>
+      </div>-->
+      <!--<div class="card">
+        <div class="card-content">
+          <q-data-table
+            :data="table"
+            :config="config"
+            :columns="columns"
+          >
+          </q-data-table>
+        </div>
+      </div>-->
+      <!--<div class="card">
+        <div class="card-container">
+          <div class="row">
+            <div class="grow-1">
+              <input v-model="username" placeholder="Nome">
+            </div>
+            <div class="grow-1">
+              <input v-model="username" placeholder="Tipo de documento">
+            </div>
+            <div class="grow-1">
+              <input v-model="username" placeholder="Validado no Workfinity">
+            </div>
+            <div class="grow-1">
+              <input v-model="username" placeholder="Email">
+            </div>
+          </div>
+          <div class="row">
+            <div class="grow-1">
+              <input v-model="username" placeholder="Número do Documento">
+            </div>
+            <div class="grow-1">
+              <input v-model="username" placeholder="Status">
+            </div>
+            <div class="grow-1">
+              <input v-model="username" placeholder="CEP">
+            </div>
+            <div class="grow-1">
+              <input v-model="username" placeholder="Contratante">
+            </div>
+          </div>
+        </div>
+      </div>-->
+    </div>
+
     <!-- Left side Drawer -->
     <q-drawer ref="drawer" swipe-only>
       <div class="list platform-delimiter">
@@ -23,222 +242,6 @@
         <q-drawer-link icon="alarm" to="/alarms">Alarms</q-drawer-link>
       </div>
     </q-drawer>
-
-    <!-- <div class="card">
-      <div class="card-content">
-        <p class="caption">Detalhes</p>
-        <div class="list">
-          <div class="row">
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Nome do Usuario: Gustavo
-                </div>
-              </div>
-            </div>
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Tipo de documento: CPF
-                </div>
-              </div>
-            </div>
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Validado no Workfinity? Sim
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Email: Gustavo@hotmail.com
-                </div>
-              </div>
-            </div>
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Número do Documento: 99638266015
-                </div>
-              </div>
-            </div>
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Validado no Workfinity? Sim
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Status: Habilitado
-                </div>
-              </div>
-            </div>
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  Contratante: BANRISUL
-                </div>
-              </div>
-              </div>
-            <div>
-              <div class="item">
-                <div class="item-content">
-                  CEP: 22666999
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
-
-  <!-- <div class="card">
-    <div class="card-content">
-      <p class="caption">Detalhes</p>
-      <div class="list">
-        <div class="row">
-
-          <div class="grow-1">
-            <div class="item">
-              <div class="item-content">
-                Nome do Usuario: Gustavo
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="item">
-              <div class="item-content">
-                Tipo de documento: CPF
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="item">
-              <div class="item-content">
-                Validado no Workfinity? Sim
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <div class="item">
-              <div class="item-content">
-                Email: Gustavo@hotmail.com
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="item">
-              <div class="item-content">
-                Número do Documento: 99638266015
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="item">
-              <div class="item-content">
-                Validado no Workfinity? Sim
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div>
-            <div class="item">
-              <div class="item-content">
-                Status:
-                 Habilitado
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="item">
-              <div class="item-content">
-                Contratante:
-                 BANRISUL
-              </div>
-            </div>
-            </div>
-          <div>
-            <div class="item">
-              <div class="item-content">
-                CEP:
-                 22666999
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> -->
-
-<div class="card">
-  <div class="card-content">
-    <p class="caption">Editar</p>
-      <div class="row gutter">
-
-        <div>
-          Nome de úsuario:
-          <input v-model="username" placeholder="Nome de úsuario">
-        </div>
-        <div>
-            Email: <input v-model="username" placeholder="Email">
-        </div>
-
-        <div>
-          Status:
-          <q-select
-            type="list"
-            v-model="select"
-            :options="selectOptions"
-          ></q-select>
-        </div>
-      </div>
-  </div>
-</div>
-
-<!-- <q-data-table
-  :data="table"
-  :config="config"
-  :columns="columns"
-  v-model= "table"
->
-<template slot="table">
-  <td>{{ table.name }}</td>
-</template>
-
-  <template slot="col-source" scope="cell">
-    <span v-if="cell.data === 'Audit'" class="label text-white bg-primary">
-      Audit
-      <q-tooltip>Some data</q-tooltip>
-    </span>
-    <span v-else class="label text-white bg-negative">{{cell.data}}</span>
-  </template>
-
-  <template slot="selection" scope="selection">
-    <button class="primary clear" @click="changeMessage(selection)">
-      <i>edit</i>
-    </button>
-    <button class="primary clear" @click="deleteRow(selection)">
-      <i>delete</i>
-    </button>
-  </template>
-
-</q-data-table> -->
 
   </q-layout>
 </template>
@@ -271,18 +274,40 @@ export default {
       },
       columns: [
         {
-          label: 'Date',
-          field: 'isodate',
-          width: '100px'
+          label: 'Nome',
+          field: 'nome'
+        },
+        {
+          label: 'Cor',
+          field: 'cor'
+        },
+        {
+          label: 'Fabricante',
+          field: 'fabricante'
         }
       ],
       table: [
-        {name: 'Christopher'},
-        {name: 'Andiro'}
+        {cor: 'branco', nome:'gol', fabricante:'ford'},
+        {cor: 'preto', nome:'uno', fabricante:'Honda' },
+        {cor: 'azul', nome:'fusca', fabricante:'Hyundai'}
       ]
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </script>
 <style>
 </style>
