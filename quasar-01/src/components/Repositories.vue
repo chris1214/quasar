0694a391c1cc5024fc2df83268c0e73a1d3f2c4b
Chrisotpher Andrio <christopher_andrio@hotmail.com>
2017-05-07 23:40:28 -0300

    Home

--- quasar-01/src/components/Repositories.vue
+++ quasar-01/src/components/Repositories.vue
@@ -24,40 +24,221 @@
       </div>
     </q-drawer>
 
+    <!-- <div class="card">
+      <div class="card-content">
+        <p class="caption">Detalhes</p>
+        <div class="list">
+          <div class="row">
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Nome do Usuario: Gustavo
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Tipo de documento: CPF
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Validado no Workfinity? Sim
+                </div>
+              </div>
+            </div>
+          </div>
 
-    <table class="q-table responsive">
-      <thead>
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-        <th>In Stock</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td data-th="Name">Item #1</td>
-        <td data-th="Price">$10.11</td>
-        <td data-th="In Stock">101</td>
-      </tr>
-      <tr>
-        <td data-th="Name">Item #2</td>
-        <td data-th="Price">$8.88</td>
-        <td class="text-right">34</td>
-      </tr>
-      <tr>
-        <td data-th="Name">Item #3</td>
-        <td data-th="Price">$0.15</td>
-        <td data-th="In Stock">1670</td>
-      </tr>
-      </tbody>
-    </table>
+          <div class="row">
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Email: Gustavo@hotmail.com
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Número do Documento: 99638266015
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Validado no Workfinity? Sim
+                </div>
+              </div>
+            </div>
+          </div>
 
-    <q-data-table
-      :data="table"
-      :config="config"
-      :columns="columns"
-    >
-    </q-data-table>
+          <div class="row">
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Status: Habilitado
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  Contratante: BANRISUL
+                </div>
+              </div>
+              </div>
+            <div>
+              <div class="item">
+                <div class="item-content">
+                  CEP: 22666999
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+  <!-- <div class="card">
+    <div class="card-content">
+      <p class="caption">Detalhes</p>
+      <div class="list">
+        <div class="row">
+
+          <div class="grow-1">
+            <div class="item">
+              <div class="item-content">
+                Nome do Usuario: Gustavo
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="item">
+              <div class="item-content">
+                Tipo de documento: CPF
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="item">
+              <div class="item-content">
+                Validado no Workfinity? Sim
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div class="item">
+              <div class="item-content">
+                Email: Gustavo@hotmail.com
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="item">
+              <div class="item-content">
+                Número do Documento: 99638266015
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="item">
+              <div class="item-content">
+                Validado no Workfinity? Sim
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div>
+            <div class="item">
+              <div class="item-content">
+                Status:
+                 Habilitado
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="item">
+              <div class="item-content">
+                Contratante:
+                 BANRISUL
+              </div>
+            </div>
+            </div>
+          <div>
+            <div class="item">
+              <div class="item-content">
+                CEP:
+                 22666999
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+<div class="card">
+  <div class="card-content">
+    <p class="caption">Editar</p>
+      <div class="row gutter">
+
+        <div>
+          Nome de úsuario:
+          <input v-model="username" placeholder="Nome de úsuario">
+        </div>
+        <div>
+            Email: <input v-model="username" placeholder="Email">
+        </div>
+
+        <div>
+          Status:
+          <q-select
+            type="list"
+            v-model="select"
+            :options="selectOptions"
+          ></q-select>
+        </div>
+      </div>
+  </div>
+</div>
+
+<!-- <q-data-table
+  :data="table"
+  :config="config"
+  :columns="columns"
+  v-model= "table"
+>
+<template slot="table">
+  <td>{{ table.name }}</td>
+</template>
+
+  <template slot="col-source" scope="cell">
+    <span v-if="cell.data === 'Audit'" class="label text-white bg-primary">
+      Audit
+      <q-tooltip>Some data</q-tooltip>
+    </span>
+    <span v-else class="label text-white bg-negative">{{cell.data}}</span>
+  </template>
+
+  <template slot="selection" scope="selection">
+    <button class="primary clear" @click="changeMessage(selection)">
+      <i>edit</i>
+    </button>
+    <button class="primary clear" @click="deleteRow(selection)">
+      <i>delete</i>
+    </button>
+  </template>
+
+</q-data-table> -->
 
   </q-layout>
 </template>
@@ -66,30 +247,42 @@
 export default {
   data () {
     return {
+      select: '',
+      selectOptions: [
+        {
+          label: '',
+          value: ''
+        },
+        {
+          label: 'Habilitado',
+          value: 'habilitado'
+        },
+        {
+          label: 'Desabilitado',
+          value: 'desabilitado'
+        }
+      ],
+      username: '',
+      password: '',
       date: '21-10-2016',
       msg: 'Some message',
-      config : {
+      config: {
         rowHeight: '50px'
       },
-      columns : [
+      columns: [
         {
           label: 'Date',
           field: 'isodate',
-          width: '100px',
-          value: 'name'
+          width: '100px'
         }
       ],
-      table : [
+      table: [
         {name: 'Christopher'},
         {name: 'Andiro'}
       ]
     }
   }
 }
-
-
-
 </script>
-
 <style>
 </style>
