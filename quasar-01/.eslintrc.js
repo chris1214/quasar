80d905e7eb885b29525930551602a75865f5cb07
Christopher andrio <christopher_andrio@hotmail.com>
2017-05-09 14:09:27 -0300

    Adicionando 'no-trailing-spaces': 0, no rules para que ele pare de reclamar de espaçoes vazios

--- quasar-01/.eslintrc.js
+++ quasar-01/.eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
+    'no-trailing-spaces': 0,
     'arrow-parens': 0,
     'one-var': 0,
     'import/first': 0,
