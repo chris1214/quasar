f1f6c170fa9caba9d11775c193614915dc68b7ca
Christopher andrio <christopher_andrio@hotmail.com>
2017-05-11 14:01:25 -0300

    Código para fazer o Vue parar de reclamar

--- quasar-01/.eslintrc.js
+++ quasar-01/.eslintrc.js
@@ -1,32 +1,16 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
   },
-  env: {
-    browser: true
-  },
-  globals: {
-    'cordova': true,
-    'DEV': true,
-    'PROD': true,
-    '__THEME': true
-  },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'no-trailing-spaces': 0,
-    'arrow-parens': 0,
-    'one-var': 0,
-    'import/first': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
